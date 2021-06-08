2e4e48ce9215ba67addf5971de6b116bdaa6dae5
yeswuchaoxin <2115114745@qq.com>
2021-06-08 18:08:09 +0800

    1.0

---
+++ src/main/java/com/nowcoder/controller/NewsController.java
@@ -0,0 +1,43 @@
+package com.nowcoder.controller;
+
+import com.nowcoder.model.*;
+import com.nowcoder.service.UserService;
+import com.nowcoder.util.ToutiaoUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.View;
+
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.VolatileImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by nowcoder on 2016/7/2.
+ */
+@Controller
+public class NewsController {
+    private static final Logger logger = LoggerFactory.getLogger(NewsController.class);
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(path = {"/news/{newsId}"}, method = {RequestMethod.GET})
+    public String newsDetail(@PathVariable("newsId") int newsId, Model model) {
+
+        return "detail";
+    }
+
+}
